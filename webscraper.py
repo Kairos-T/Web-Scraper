ec415e354b32764ebd07871a5920303474268611
Kairos Tay <80029462+Kairos-T@users.noreply.github.com>
2023-04-16 18:18:24 +0800

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -8,5 +8,13 @@ if response.status_code == 200:
     soup = BeautifulSoup(response.content, "html.parser")
     title = soup.find("title").string
     print("Page title:", title)
+
+    content_choice = input("Do you want to retrieve the content? (y/n): ")
+    if content_choice.lower() == "y":
+        content = soup.get_text()
+        print("Page content:\n", content)
+    else:
+        print("Content retrieval skipped.")
+
 else:
-    print("Failed to retrieve page")
+    print("Failed to retrieve page.")
